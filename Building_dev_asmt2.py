3561376c85bd67e2eef993a1d931b3e519462ea3
Kobbysaah1 <103180468+Kobbysaah1@users.noreply.github.com>
2022-07-19 14:42:40 +0000

    Update Building_dev_asmt2.py

--- Building_dev_asmt2.py
+++ Building_dev_asmt2.py
@@ -9,14 +9,10 @@ num = int(input("Enter a number:  "))
 def asmt_1():
 	print("\nAssignment 1\n")
 	for i in range(num):
-		print("  "*((num-i)//2) +"*"*(i+1))
-	return ""	
-	
+		print(" "*((num-i)//2) +"*"*(i+1))
+	return ""		
 print(asmt_1())
 	
-
-
-
 def asmt_2():
 	print("".center(50,"_"))
 	print("\nAssignment 2\n")
@@ -25,9 +21,6 @@ def asmt_2():
 	print()
 	return ""		
 print(asmt_2())
-
-
-
 
 def asmt_3():
 	print("".center(50,"_"))
@@ -39,22 +32,18 @@ def asmt_3():
 		for j in range(num):
 			#anti diagonal
 			if j+i ==num-1:
-				print("*",end= "  ")
+				print("*",end= " ")
 			else:
-				print("  ", end= "  ")
+				print(" ", end= " ")
 		#2nd square		
 		for j in range(i):# rm j=0 column		
 			if j==i-1:#new diagonal 
-				print("*",end="  ")
+				print("*",end=" ")
 			else:
-				print("  ",end="  ")
+				print(" ",end=" ")
 		print()
-	return ""
-	
+	return ""	
 print(asmt_3())
-
-
-
 
 def asmt_4():
 	print("".center(50,"_"))
@@ -62,23 +51,19 @@ def asmt_4():
 	for i in range(num):
 		for j in range(num):
 			if j+i==num-1:
-				print("*",end="  ")		
+				print("*",end=" ")		
 			elif j==num-1:
-				print(i,end="  ")
+				print(i,end=" ")
 			else:
-				print("  ",end="  ")
-
+				print(" ",end=" ")
 		for j in range(i): 
 			if j==i-1:
-				print("*",end="  ")
+				print("*",end=" ")
 			else:
-				print("  ",end="  ")
+				print(" ",end=" ")
 		print()
-	return ""
-			
+	return ""			
 print(asmt_4())
-
-
 
 def asmt_5():
 	print("".center(50,"_"))
@@ -101,7 +86,3 @@ def asmt_5():
     #runs the nxt iteration of main for loop on the next line
 	return ""
 print(asmt_5())
-
-
-
-
