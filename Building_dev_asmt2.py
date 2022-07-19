39fa9485d94690967843659ddadc23c84e3c5af7
Kobbysaah1 <103180468+Kobbysaah1@users.noreply.github.com>
2022-07-19 13:43:56 +0000

    Update Building_dev_asmt2.py

--- Building_dev_asmt2.py
+++ Building_dev_asmt2.py
@@ -21,8 +21,7 @@ def asmt_2():
 	print("".center(50,"_"))
 	print("\nAssignment 2\n")
 	for i in range(num):
-		for j in range(i+1):
-			print("*",end= " ")
+			print("*"*(i+1))
 	print()
 	return ""		
 print(asmt_2())
